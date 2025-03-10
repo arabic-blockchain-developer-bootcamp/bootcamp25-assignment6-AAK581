3679f8f00fe9de8eb44f2cdcf042ea29af6b67c4
AAK581 <adhamahmad@outlook.com>
2025-03-10 11:51:33 +0200

    3

--- src/Assignment6.sol
+++ src/Assignment6.sol
@@ -1,16 +1,18 @@
+
 // SPDX-License-Identifier: UNLICENSED
 pragma solidity ^0.8.13;
 
 contract Assignment6 {
     // 1. Declare an event called `FundsDeposited` with parameters: `sender` and `amount`
-
+    event FundsDeposited(address indexed sender, uint amount);
     // 2. Declare an event called `FundsWithdrawn` with parameters: `receiver` and `amount`
-
+    event FundsWithdrawn(address indexed receiver, uint amount);
     // 3. Create a public mapping called `balances` to tracker users balances
-
+    mapping(address => uint) public balances;
     // Modifier to check if sender has enough balance
     modifier hasEnoughBalance(uint amount) {
         // Fill in the logic using require
+        require(balances[msg.sender] > amount, "Not enough funds");
         _;
     }
 
@@ -18,9 +20,10 @@ contract Assignment6 {
     // This function should:
     // - Be external and payable
     // - Emit the `FundsDeposited` event
-    function deposit() {
+    function deposit() external payable{
         // increment user balance in balances mapping 
-
+        balances[msg.sender] += msg.value;
+        emit FundsDeposited(msg.sender, balances[msg.sender]);
         // emit suitable event
     }
 
@@ -30,21 +33,21 @@ contract Assignment6 {
     // - Take one parameter: `amount`
     // - Use the `hasEnoughBalance` modifier
     // - Emit the `FundsWithdrawn` event
-    function withdraw() {
+    function withdraw(uint amount) external hasEnoughBalance(amount){
         // decrement user balance from balances mapping 
-
+        balances[msg.sender] -= amount;
         // send tokens to the caller
-
+        payable(msg.sender).transfer(amount);
         // emit suitable event
-
+        emit FundsWithdrawn(msg.sender, balances[msg.sender]);
     }
 
     // Function to check the contract balance
     // This function should:
     // - Be public and view
     // - Return the contract's balance
-    function getContractBalance() {
+    function getContractBalance() public view returns(uint){
         // return the balance of the contract
-
+        return(address(this).balance);
     }
 }
